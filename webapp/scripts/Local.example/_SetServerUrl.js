e5483b3655a1e62e8493dbab6d507b9925189005
Paul Vauterin <pvaut@gmail.com>
2013-04-16 14:11:35 +0100

    Different 'local.example'

--- webapp/scripts/Local.example/_SetServerUrl.js
+++ webapp/scripts/Local.example/_SetServerUrl.js
@@ -1,2 +1,3 @@
 //serverUrl = "http://localhost:8000/app01";
-serverUrl="http://localhost/app"; 
+//serverUrl="http://localhost/app";
+serverUrl = "http://panoptes.cggh.org/sandbox/dqx_server30/app";
