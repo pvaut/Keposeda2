854b7d079ffc37fa7ef2c85076e29715a067b382
Paul Vauterin <pvaut@gmail.com>
2013-04-16 14:10:25 +0100

    Remove 'local' folder

--- webapp/scripts/Local/_SetAnalyticsId.js
+++
@@ -1,9 +0,0 @@
-googleAnalyticsId="UA-38243754-1";
-localhost=true;
-
-function setAnalyticsOptions(localhost, gaq) {
-	if (localhost) {
-  		gaq.push(['_setDomainName', 'none']);
-  		gaq.push(['_setAllowLinker', true]);   
-	}
-}
--- webapp/scripts/Local/_SetServerUrl.js
+++
@@ -1,6 +0,0 @@
-﻿
-
-//serverUrl = "http://localhost:8000/app01";
-
-//serverUrl = "http://panoptes.cggh.org/sandbox/dqx_server_pfx/app";
-serverUrl = "http://panoptes.cggh.org/sandbox/dqx_server30/app";
